752290f28b4a1e03e3a182ec6dc701db8c022494
Daniel Saper <dansaper@umich.edu>
2015-07-09 11:23:06 -0400

    Initial Commit

---
+++ FaneuliHall/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FaneuliHall
+//
+//  Created by Daniel Saper on 7/9/15.
+//  Copyright (c) 2015 Daniel Saper. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
